14ac44f339a80d598f81ebb7bda27f5225e4943c
joohee.jeong <joohee.jeong@emotion.co.kr>
2020-09-11 18:34:23 +0900

    1차원 배열 200911

---
+++ src/com/github/torissi/algorithm_1array/OneArray1_200911.java
@@ -0,0 +1,32 @@
+package com.github.torissi.algorithm_1array;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class OneArray1_200911 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[a];
+        for (int i=0; i<a; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        System.out.println(arr[0] + " " + arr[a-1]);
+    }
+}
+
+//Java를 사용하고 있다면, Scanner와 System.out.println 대신 BufferedReader와 BufferedWriter를 사용할 수 있다.
+// BufferedWriter.flush는 맨 마지막에 한 번만 하면 된다.
+
+// 문제에서 입력이 한 줄로 나온다면 진짜 한 줄 입력을 해서 할 수 있도록 해야함
+//나는 처음에 기존에 하던 방식 처럼 한 줄로 받아서 StringTokenizer로 띄어쓰기 기준으로 잘라서 써서 런타임 에러가 났음
+
+//https://www.acmicpc.net/problem/10818
