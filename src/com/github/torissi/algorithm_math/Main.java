03cd0fbcfa09553b9bf722ba67cc2f3e800fc16a
joohee.jeong <joohee.jeong@emotion.co.kr>
2020-09-16 15:31:19 +0900

    수학1 200916

---
+++ src/com/github/torissi/algorithm_math/Main.java
@@ -0,0 +1,23 @@
+package com.github.torissi.algorithm_math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+
+
+    }
+}
+
+//Java를 사용하고 있다면, Scanner와 System.out.println 대신 BufferedReader와 BufferedWriter를 사용할 수 있다.
+// BufferedWriter.flush는 맨 마지막에 한 번만 하면 된다.
+
+// 문제에서 입력이 한 줄로 나온다면 진짜 한 줄 입력을 해서 할 수 있도록 해야함
+//나는 처음에 기존에 하던 방식 처럼 한 줄로 받아서 StringTokenizer로 띄어쓰기 기준으로 잘라서 써서 런타임 에러가 났음
+
+//https://www.acmicpc.net/problem/2839
---
+++ src/com/github/torissi/algorithm_math/Math1_200916.java
@@ -0,0 +1,31 @@
+package com.github.torissi.algorithm_math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Math1_200916 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long a = Integer.parseInt(st.nextToken());
+        long b = Integer.parseInt(st.nextToken());
+        long c = Integer.parseInt(st.nextToken());
+
+        if ((c-b) > 0) {
+            System.out.println((a / (c - b)) + 1);
+        } else {
+            System.out.println("-1");
+        }
+    }
+}
+
+//Java를 사용하고 있다면, Scanner와 System.out.println 대신 BufferedReader와 BufferedWriter를 사용할 수 있다.
+// BufferedWriter.flush는 맨 마지막에 한 번만 하면 된다.
+
+// 문제에서 입력이 한 줄로 나온다면 진짜 한 줄 입력을 해서 할 수 있도록 해야함
+//나는 처음에 기존에 하던 방식 처럼 한 줄로 받아서 StringTokenizer로 띄어쓰기 기준으로 잘라서 써서 런타임 에러가 났음
+
+//https://www.acmicpc.net/problem/1712
