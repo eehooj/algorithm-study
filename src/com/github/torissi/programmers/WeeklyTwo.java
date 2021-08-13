3fc13214061194b7d1c719316c47b42bb417ab94
toriSSi <jooh21020@gmail.com>
2021-08-14 01:18:28 +0900

    위클리2 - 속도 줄이기 20210814

--- src/com/github/torissi/programmers/WeeklyTwo.java
+++ src/com/github/torissi/programmers/WeeklyTwo.java
@@ -1,91 +1,50 @@
 package com.github.torissi.programmers;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 public class WeeklyTwo {
     public String solution(int[][] scores) {
         int length = scores.length;
-        boolean isA = false;
         StringBuilder grade = new StringBuilder();
 
         for (int i = 0; i < length; i++) { //1
-            List<Integer> score = new ArrayList<>();
-            int maxj = scores[i][i];
-            int maxIndex = i;
-            int minj = scores[i][i];
-            int minIndex = i;
+            int sum = 0;
+            int maxj = 0;
+            int minj = 101;
+            int divide = length;
 
             for (int j = 0; j < length; j++) { //2
-                if (maxj < scores[j][i]) {
-                    maxIndex = j;
-                    maxj = scores[j][i];
-                } else if (minj > scores[j][i]) {
-                    minIndex = j;
-                    minj = scores[j][i];
-                }
+                int score = scores[j][i];
+                sum += score; 
 
-                score.add(scores[j][i]);
-            }
-
-            if ((maxIndex == i || minIndex == i)) {
-                if (Collections.frequency(score, score.get(i)) < 2) {
-                    score.set(i, 0);
-                    isA = true;
+                if (i != j) { // 평균 계산시 오직 내가 준 내 점수가 min, max 값일 때에만 제외
+                              // => 동일 min, max 값이 있다면 제외 X
+                              // 동일한 min, max 값이 있을 수 있기 때문에 기준이 되는 scores[i][i]의 값은 빼고 min, max를 구한다
+                              // scores[i][i]와 같은 값이 있을 경우에는 평균 계산에 사용 하기 때문에 일단 min, max 구하고 밑에서 비교
+                              // 이 부분이 문제 쉽게 푸는 핵심인듯?
+                    if (maxj < scores[j][i]) {
+                        maxj = scores[j][i];
+                    } else if (minj > scores[j][i]) {
+                        minj = scores[j][i];
+                    }
                 }
             }
 
-            double ave = 0.0;
-            if (isA) {
-                ave = (double) score.stream().mapToInt(Integer::intValue).sum() / (length - 1); //3
-            } else {
-                ave = (double) score.stream().mapToInt(Integer::intValue).sum() / length;
+            if (scores[i][i] > maxj || scores[i][i] < minj) {
+                sum -= scores[i][i];
+                divide--;
             }
+
+            double ave = (double) sum / divide;
 
             if (ave >= 90.0) grade.append("A");
             else if (ave >= 80.0) grade.append("B");
             else if (ave >= 70.0) grade.append("C");
             else if (ave >= 50.0) grade.append("D");
             else grade.append("F");
-
-            isA = false;
         }
 
         return grade.toString();
     }
 }
-
-/*class Solution {
-    public String solution(int[][] scores) {
-        StringBuilder builder = new StringBuilder();
-        for(int i=0; i<scores.length; i++) {
-            int max = 0;
-            int min = 101;
-            int sum = 0;
-            int divide = scores.length;
-            for(int j=0; j<scores.length; j++) {
-                int score = scores[j][i];
-                if(i != j) {
-                    if(score < min) {
-                        min = score;
-                    }
-                    if(score > max) {
-                        max = score;
-                    }
-                }
-                sum += score;
-            }
-            if(scores[i][i] < min || scores[i][i] > max) {
-                sum -= scores[i][i];
-                divide--;
-            }
-            double score = (double) sum / divide;
-            builder.append(score >= 90 ? "A" : score >= 80 ? "B" : score >= 70 ? "C" : score >= 50 ? "D" : "F" );
-        }
-        return builder.toString();
-    }
-}*/
 
 /*문제 설명
 대학 교수인 당신은, 상호평가를 통하여 학생들이 제출한 과제물에 학점을 부여하려고 합니다. 아래는 0번부터 4번까지 번호가 매겨진 5명의 학생들이 자신과 다른 학생의 과제를 평가한 점수표입니다.
@@ -158,24 +117,24 @@ No.	0	1	2
 2번 학생이 자기 자신을 평가한 100점은 [90, 38, 100]에서 유일한 최고점이므로, 평균을 구할 때 제외합니다.
 */
 
-/*테스트 1 〉	통과 (1.11ms, 70.3MB)
-테스트 2 〉	통과 (1.74ms, 60.8MB)
-테스트 3 〉	통과 (1.42ms, 59.1MB)
-테스트 4 〉	통과 (2.00ms, 62.4MB)
-테스트 5 〉	통과 (1.95ms, 74.5MB)
-테스트 6 〉	통과 (1.86ms, 74.5MB)
-테스트 7 〉	통과 (1.46ms, 73.4MB)
-테스트 8 〉	통과 (1.27ms, 67.5MB)
-테스트 9 〉	통과 (1.76ms, 72.5MB)
-테스트 10 〉	통과 (1.65ms, 71.6MB)
-테스트 11 〉	통과 (1.14ms, 60.6MB)
-테스트 12 〉	통과 (1.73ms, 71.7MB)
-테스트 13 〉	통과 (1.34ms, 59.5MB)
-테스트 14 〉	통과 (1.58ms, 69.5MB)
-테스트 15 〉	통과 (1.69ms, 72.9MB)
-테스트 16 〉	통과 (1.22ms, 70.1MB)
-테스트 17 〉	통과 (1.42ms, 70.9MB)
-테스트 18 〉	통과 (2.08ms, 60.9MB)
-테스트 19 〉	통과 (1.80ms, 62.1MB)
-테스트 20 〉	통과 (1.45ms, 70.2MB)
-테스트 21 〉	통과 (1.15ms, 73.4MB)*/
+/*테스트 1 〉	통과 (0.03ms, 57.9MB)
+테스트 2 〉	통과 (0.03ms, 69.8MB)
+테스트 3 〉	통과 (0.03ms, 69.6MB)
+테스트 4 〉	통과 (0.03ms, 71.1MB)
+테스트 5 〉	통과 (0.04ms, 73.8MB)
+테스트 6 〉	통과 (0.03ms, 69MB)
+테스트 7 〉	통과 (0.03ms, 61.8MB)
+테스트 8 〉	통과 (0.05ms, 60MB)
+테스트 9 〉	통과 (0.03ms, 73.3MB)
+테스트 10 〉	통과 (0.05ms, 74.9MB)
+테스트 11 〉	통과 (0.04ms, 77.3MB)
+테스트 12 〉	통과 (0.06ms, 57.2MB)
+테스트 13 〉	통과 (0.02ms, 71.6MB)
+테스트 14 〉	통과 (0.05ms, 74MB)
+테스트 15 〉	통과 (0.04ms, 72.8MB)
+테스트 16 〉	통과 (0.04ms, 73.3MB)
+테스트 17 〉	통과 (0.03ms, 69.9MB)
+테스트 18 〉	통과 (0.03ms, 71.7MB)
+테스트 19 〉	통과 (0.03ms, 73.2MB)
+테스트 20 〉	통과 (0.04ms, 74MB)
+테스트 21 〉	통과 (0.03ms, 68MB)*/
