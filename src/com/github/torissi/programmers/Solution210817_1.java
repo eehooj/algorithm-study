9a9ba9ef52d8fddc6ff7c8d0af3321f331c5b58c
toriSSi <jooh21020@gmail.com>
2021-08-17 20:13:04 +0900

    20210816 - 평균 구하기 stream vs for

--- src/com/github/torissi/programmers/Solution210817_1.java
+++ src/com/github/torissi/programmers/Solution210817_1.java
@@ -1,5 +1,7 @@
 package com.github.torissi.programmers;
 
+import java.util.Arrays;
+
 public class Solution210817_1 {
     public double solution(int[] arr) {
         int sum = 0;
@@ -41,3 +43,26 @@ arr	return
 테스트 14 〉	통과 (0.02ms, 69.5MB)
 테스트 15 〉	통과 (0.03ms, 72.1MB)
 테스트 16 〉	통과 (0.02ms, 74.8MB)*/
+
+/*return Arrays.stream(arr).average().orElse(0);
+테스트 1 〉	통과 (3.07ms, 74.1MB)
+테스트 2 〉	통과 (2.28ms, 75.2MB)
+테스트 3 〉	통과 (2.08ms, 63.1MB)
+테스트 4 〉	통과 (2.02ms, 69.5MB)
+테스트 5 〉	통과 (1.82ms, 69.5MB)
+테스트 6 〉	통과 (2.02ms, 68.8MB)
+테스트 7 〉	통과 (2.24ms, 69.6MB)
+테스트 8 〉	통과 (1.86ms, 60.9MB)
+테스트 9 〉	통과 (2.06ms, 73.9MB)
+테스트 10 〉	통과 (2.46ms, 61.2MB)
+테스트 11 〉	통과 (2.07ms, 72.6MB)
+테스트 12 〉	통과 (2.22ms, 58.6MB)
+테스트 13 〉	통과 (1.98ms, 73.1MB)
+테스트 14 〉	통과 (1.96ms, 73.5MB)
+테스트 15 〉	통과 (1.74ms, 71.6MB)
+테스트 16 〉	통과 (1.91ms, 73.7MB)
+
+=> 코드는 한 줄로 짧아지나 시간이 너무 오래 걸린다. stream이 for문 보다 오래 걸리나?
+=> 찾아보니 stream이 더 오래 걸림! 다량의 데이터를 처리할 때는 for문을 쓰는게 좋을 것 같다.
+=> 그 외에는 stream이 훨씬 깔끔하고 간결.. 블로그 글 보니까 map과 함께 쓸때는 stream이 더 좋은 듯!
+*/
