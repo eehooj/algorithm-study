c020de6af206107f243f641ec9318c4314093e60
toriSSi <jooh21020@gmail.com>
2021-09-07 17:49:20 +0900

    20210908 - 복서 정렬하기 (1등 답 주석 -> 참고해서 다시 해보기)

--- src/com/github/torissi/programmers/level1/Solution210907_3.java
+++ src/com/github/torissi/programmers/level1/Solution210907_3.java
@@ -144,3 +144,31 @@ weights	head2head	result
 테스트 11 〉	통과 (149.08ms, 111MB)
 테스트 12 〉	통과 (112.80ms, 121MB)
 */
+
+/*
+int len = weights.length;
+        int[][] rank = new int[len][4];
+        for(int i = 0; i < len; i++) {
+            int w = weights[i], cnt = 0, win = 0, over = 0;
+            for(int j = 0; j < len; j++) {
+                char c = head2head[i].charAt(j);
+                cnt += c == 'N' ? 0 : 1;
+                win += c == 'W' ? 1 : 0;
+                over += c == 'W' && weights[i] < weights[j] ? 1 : 0;
+            }
+            rank[i][0] = i + 1;
+            rank[i][1] = (int)((double)win / cnt * 10000000);
+            rank[i][2] = over;
+            rank[i][3] = weights[i];
+        }
+        Arrays.sort(rank, (a, b) -> {
+            if(a[1] != b[1]) return b[1] - a[1];
+            if(a[2] != b[2]) return b[2] - a[2];
+            if(a[3] != b[3]) return b[3] - a[3];
+            return a[0] - b[0];
+        });
+        int[] answer = new int[len];
+        for(int i = 0; i < len; i++) answer[i] = (int)rank[i][0];
+        return answer;
+=> 좋아요 많은 답.. 더 빠름.. 다시 해보기!
+*/
