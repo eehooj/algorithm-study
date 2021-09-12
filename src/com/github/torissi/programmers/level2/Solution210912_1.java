114f1a715dea5ef19c1370fcb57bb66d01a95b94
toriSSi <jooh21020@gmail.com>
2021-09-12 13:44:25 +0900

    20210912 - 땅따먹기 (Dynamic Programming)

--- src/com/github/torissi/programmers/level2/Solution210912_1.java
+++ src/com/github/torissi/programmers/level2/Solution210912_1.java
@@ -1,39 +1,38 @@
 package com.github.torissi.programmers.level2;
 
-import java.util.Arrays;
-
 public class Solution210912_1 {
 
-    public static int solution(int[][] land) {
-        int answer = 0;
-        int prevCol = -1;
+     int solution(int[][] land) {
+        int length = land.length;
 
-        for (int[] temp : land) {
-            int max = temp[0];
-
-            int maxCol = 0;
-
-            for (int j = 0; j < 4; j++) {
-                if (prevCol != j) {
-                    if (temp[j] > max) {
-                        max = temp[j];
-                        maxCol = j;
-                    }
-                }
-            }
-
-            prevCol = maxCol;
-            answer += max;
+        for (int i = 1; i < length; i++) {
+            land[i][0] += maxValue(land[i - 1][1], land[i - 1][2], land[i - 1][3]);
+            land[i][1] += maxValue(land[i - 1][0], land[i - 1][2], land[i - 1][3]);
+            land[i][2] += maxValue(land[i - 1][1], land[i - 1][0], land[i - 1][3]);
+            land[i][3] += maxValue(land[i - 1][1], land[i - 1][2], land[i - 1][0]);
         }
 
-        return answer;
-    }
+        return maxValue(land[length - 1]);
+     }
 
-    public static void main(String[] args) {
+     int maxValue(int[] ints) {
+        int max = 0;
+        for (int num : ints) {
+            max = Math.max(max, num);
+        }
+
+        return max;
+     }
+
+     int maxValue(int i, int i1, int i2) {
+        return Math.max(Math.max(i, i1), i2);
+     }
+
+     public static void main(String[] args) {
         //int[][] land = {{1,2,3,5}, {5,6,7,8}, {4,3,2,1}}; //15
         int[][] land = {{1,1,1,1}, {2,2,2,3}, {3,3,3,6}, {4,4,4,7}}; //14
-        solution(land);
-    }
+        //solution(land);
+     }
 }
 
 /* 땅따먹기
@@ -65,5 +64,30 @@ land	answer
 */
 
 /*
-
+정확성  테스트
+테스트 1 〉	통과 (0.42ms, 61.7MB)
+테스트 2 〉	통과 (0.39ms, 72.2MB)
+테스트 3 〉	통과 (0.38ms, 59.2MB)
+테스트 4 〉	통과 (0.37ms, 58.4MB)
+테스트 5 〉	통과 (0.36ms, 72.5MB)
+테스트 6 〉	통과 (0.38ms, 75MB)
+테스트 7 〉	통과 (0.36ms, 71.3MB)
+테스트 8 〉	통과 (0.37ms, 58MB)
+테스트 9 〉	통과 (0.36ms, 72.7MB)
+테스트 10 〉	통과 (0.38ms, 60.6MB)
+테스트 11 〉	통과 (0.36ms, 60.5MB)
+테스트 12 〉	통과 (0.36ms, 71.1MB)
+테스트 13 〉	통과 (0.39ms, 73.8MB)
+테스트 14 〉	통과 (0.38ms, 59.1MB)
+테스트 15 〉	통과 (0.39ms, 61.5MB)
+테스트 16 〉	통과 (0.39ms, 61.1MB)
+테스트 17 〉	통과 (0.42ms, 58.2MB)
+테스트 18 〉	통과 (0.37ms, 73.7MB)
+효율성  테스트
+테스트 1 〉	통과 (16.01ms, 86.1MB)
+테스트 2 〉	통과 (16.39ms, 85.6MB)
+테스트 3 〉	통과 (16.75ms, 85.4MB)
+테스트 4 〉	통과 (15.92ms, 85.6MB)
+=> Dynamic Programming
+동적 계획법이란 특정 범위까지의 값을 구하기 위해 그것과 다른 범위까지의 값을 이용하여 효율적으로 값을 구하는 알고리즘 설계 기법
 */
