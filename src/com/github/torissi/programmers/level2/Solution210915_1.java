638f2a2ade5c0f53afa010f76c2b4fe47ba3f38a
toriSSi <jooh21020@gmail.com>
2021-09-15 15:47:17 +0900

    20210915 - 다음 큰 숫자(Integer.bitCount())

--- src/com/github/torissi/programmers/level2/Solution210915_1.java
+++ src/com/github/torissi/programmers/level2/Solution210915_1.java
@@ -5,31 +5,16 @@ public class Solution210915_1 {
     public int solution(int n) {
         int answer = 0;
         int num = n + 1;
-        String nm = Integer.toBinaryString(n);
-        int count = checkCount(n);
+        int count = Integer.bitCount(n);
 
         for (int i = num; i < 1000000; i++) {
-            int compare = checkCount(i);
-            if (count == compare) {
+            if (count == Integer.bitCount(i)) {
                 answer = i;
                 break;
             }
         }
 
         return answer;
-    }
-
-    private int checkCount(int n) {
-        String nm = Integer.toBinaryString(n);
-        int count = 0;
-
-        for (String a : nm.split("")) {
-            if (a.equals("1")) {
-                count++;
-            }
-        }
-
-        return count;
     }
 
     public static void main(String[] args) {
@@ -62,25 +47,25 @@ n	result
 
 /*
 정확성  테스트
-테스트 1 〉	통과 (0.27ms, 60MB)
-테스트 2 〉	통과 (0.16ms, 70.5MB)
-테스트 3 〉	통과 (0.22ms, 60MB)
-테스트 4 〉	통과 (0.18ms, 57.4MB)
-테스트 5 〉	통과 (0.33ms, 62MB)
-테스트 6 〉	통과 (0.24ms, 69.4MB)
-테스트 7 〉	통과 (0.45ms, 71.4MB)
-테스트 8 〉	통과 (0.18ms, 70.1MB)
-테스트 9 〉	통과 (0.40ms, 69.3MB)
-테스트 10 〉	통과 (0.45ms, 74.6MB)
-테스트 11 〉	통과 (0.26ms, 66.8MB)
-테스트 12 〉	통과 (0.22ms, 70MB)
-테스트 13 〉	통과 (0.19ms, 60.9MB)
-테스트 14 〉	통과 (0.20ms, 63.1MB)
+테스트 1 〉	통과 (0.02ms, 69MB)
+테스트 2 〉	통과 (0.02ms, 70.7MB)
+테스트 3 〉	통과 (0.02ms, 71.5MB)
+테스트 4 〉	통과 (0.02ms, 59.7MB)
+테스트 5 〉	통과 (0.03ms, 70MB)
+테스트 6 〉	통과 (0.02ms, 58.2MB)
+테스트 7 〉	통과 (0.02ms, 73MB)
+테스트 8 〉	통과 (0.03ms, 71.5MB)
+테스트 9 〉	통과 (0.02ms, 66.5MB)
+테스트 10 〉	통과 (0.02ms, 70.6MB)
+테스트 11 〉	통과 (0.01ms, 70.9MB)
+테스트 12 〉	통과 (0.02ms, 59MB)
+테스트 13 〉	통과 (0.02ms, 68.4MB)
+테스트 14 〉	통과 (0.02ms, 75MB)
 효율성  테스트
-테스트 1 〉	실패 (시간 초과)
-테스트 2 〉	실패 (시간 초과)
-테스트 3 〉	실패 (시간 초과)
-테스트 4 〉	실패 (시간 초과)
-테스트 5 〉	실패 (시간 초과)
-테스트 6 〉	실패 (시간 초과)
+테스트 1 〉	통과 (0.02ms, 52.5MB)
+테스트 2 〉	통과 (0.02ms, 52.2MB)
+테스트 3 〉	통과 (0.02ms, 52.7MB)
+테스트 4 〉	통과 (0.02ms, 52.1MB)
+테스트 5 〉	통과 (0.03ms, 52.4MB)
+테스트 6 〉	통과 (0.02ms, 51.6MB)
 */
