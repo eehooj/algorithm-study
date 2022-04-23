b24d1b056440b2bcc4333f37c273a03599d1c63c
toriSSi <jooh21020@gmail.com>
2022-04-23 23:56:41 +0900

    20220423 - 국비지원 1주차 - 가장 큰 수

--- src/com/github/torissi/programmers/WeeklyOne_1.java
+++ src/com/github/torissi/programmers/WeeklyOne_1.java
@@ -1,26 +1,38 @@
 package com.github.torissi.programmers;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.stream.Collectors;
 
 public class WeeklyOne_1 {
     public static String solution(int[] numbers) {
         int length = numbers.length;
         String[] arr = new String[length];
+        StringBuilder answer = new StringBuilder();
 
         for (int i = 0; i < length; i++) {
             arr[i] = String.valueOf(numbers[i]);
         }
 
-        String answer = Arrays.stream(arr)
+        /*answer = Arrays.stream(arr)
                 .sorted((a, b) -> (b + a).compareTo(a + b))
                 .collect(Collectors.joining());
 
         if (answer.startsWith("0")) {
             answer = "0";
+        }*/
+
+        Arrays.sort(arr, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
+
+        for (String item : arr) {
+            answer.append(item);
         }
 
-        return answer;
+        if (answer.toString().startsWith("0")) {
+            return "0";
+        }
+
+        return answer.toString();
     }
 
     public static void main(String[] args) {
