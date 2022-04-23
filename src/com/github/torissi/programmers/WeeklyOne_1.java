f4d6fbba36e0cabbf89e7ee02dcef83a9b1d7df4
toriSSi <jooh21020@gmail.com>
2022-04-23 23:58:17 +0900

    20220423 - 국비지원 1주차 - 가장 큰 수

--- src/com/github/torissi/programmers/WeeklyOne_1.java
+++ src/com/github/torissi/programmers/WeeklyOne_1.java
@@ -1,8 +1,6 @@
 package com.github.torissi.programmers;
 
 import java.util.Arrays;
-import java.util.Comparator;
-import java.util.stream.Collectors;
 
 public class WeeklyOne_1 {
     public static String solution(int[] numbers) {
@@ -36,7 +34,7 @@ public class WeeklyOne_1 {
     }
 
     public static void main(String[] args) {
-        int [] arr = {0, 0, 0, 0, 0};
+        int [] arr = {3, 30, 34, 5, 9};
 
         solution(arr);
     }
