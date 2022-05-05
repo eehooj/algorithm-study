d5e4e5b449b0a8afe49aec8bec6d856572e9ffa4
toriSSi <jooh21020@gmail.com>
2022-05-05 20:52:33 +0900

    20220505 - 국비지원 3주차 - 위장

---
+++ src/com/github/torissi/programmers/WeeklyThree_1.java
@@ -0,0 +1,29 @@
+package com.github.torissi.programmers;
+
+import java.util.HashMap;
+
+public class WeeklyThree_1 {
+
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (String[] item : clothes) {
+            map.put(item[1], map.getOrDefault(item[1], 0) + 1);
+        }
+
+        for (Integer item : map.values()) {
+            answer *= (item + 1);
+        }
+
+        return answer;
+    }
+
+}
+
+
+/*
+* 1. 매일 다른 옷 조합
+* 2. 2차원 배열
+* 3. 서로 다른 옷의 조합의 수 리턴
+* */
