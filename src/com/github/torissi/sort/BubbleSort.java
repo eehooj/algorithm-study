c012ccc729d939fcc5baac5018956e12692867ef
toriSSi <jooh21020@gmail.com>
2022-07-05 22:35:57 +0900

    20220704 자바(Java) 알고리즘 문제풀이 : 코딩테스트 대비 - 버블정렬

---
+++ src/com/github/torissi/sort/BubbleSort.java
@@ -0,0 +1,37 @@
+package com.github.torissi.sort;
+
+import java.util.Scanner;
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        int[] answer = solution(n, arr);
+
+        for (int item : answer) {
+            System.out.print(item + " ");
+        }
+    }
+
+    private static int[] solution(int n, int[] arr) {
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+
+        }
+
+        return arr;
+    }
+}
